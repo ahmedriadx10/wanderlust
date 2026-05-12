c97dec569426444935d8e6c182fd33511c0d8d8f
ahmedriadx10 <infinitecoderx02@gmail.com>
2026-05-12 23:37:17 +0600

    sign up page added

--- src/app/(auth)/signup/page.jsx
+++ src/app/(auth)/signup/page.jsx
@@ -1,9 +1,25 @@
+import { SignUpForm } from "@/components/client-components/SignUpForm";
 
 const SignupPage = () => {
   return (
-    <div>
-      <h2>Signup Page</h2>
-    </div>
+    <seciton>
+
+<div className="text-center space-y-5 mx-auto">
+<h2 className="text-4xl">Create Account</h2>
+<p className="text-(--primaryText) text-xl">Start your adventure with Wanderlust</p>
+
+</div>
+
+
+
+
+
+<SignUpForm/>
+
+
+
+
+    </seciton>
   );
 };
 
